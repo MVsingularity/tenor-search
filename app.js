bf79c108e7e39b4526eb46845f4f6b97aa6ca7fa
MVsingularity <60405343+MVsingularity@users.noreply.github.com>
2022-08-27 10:48:35 -0700

    added search form

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const app = express();
 
 const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  res.render('hello-gif', { gifUrl })
-})
-
-app.get('/greetings/:name', (req, res) => {
+app.get('/greetings/name', (req, res) => {
   const name = req.params.name;
   res.render('greetings', { name });
 })
+
+app.get('/', (req, res) => {
+  console.log(req.query)
+  res.render('home')
+})
+
 
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
